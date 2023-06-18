4a0082722434fe00b98422de326a4acf42442202
ishani131 <ishani.chowdhury2020@vitstudent.ac.in>
2023-06-18 23:28:12 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(window).on('scroll', function(){
+        var scroll = $(window).scrollTop();
+        
+        if(scroll >= 50){
+            $(".sticky").addClass("stickyadd");
+        }else{
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+});
